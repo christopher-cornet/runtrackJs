c70a93cf00e673cae1b0586b2816509105c94b3a
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-06-26 20:45:35 +0200

    Job 1-6 + Begin Job 7

---
+++ job05/index.js
@@ -0,0 +1,9 @@
+let jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+function afficherjourssemaines() {
+    for (let i = 0; i < jourssemaines.length; i++) {
+        console.log(jourssemaines[i]);
+    }
+}
+
+afficherjourssemaines();
