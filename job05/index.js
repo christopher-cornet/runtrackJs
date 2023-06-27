0769c780268a187a9d316046fb65f9e625b6f597
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-06-27 10:59:22 +0200

    Delete job05 directory

--- job05/index.js
+++
@@ -1,9 +0,0 @@
-let jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
-
-function afficherjourssemaines() {
-    for (let i = 0; i < jourssemaines.length; i++) {
-        console.log(jourssemaines[i]);
-    }
-}
-
-afficherjourssemaines();
