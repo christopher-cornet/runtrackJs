ecc0731c7e902f5d51b33e3f102fbcbb158328e0
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-06-27 23:58:02 +0200

    Add files via upload

---
+++ jour02/job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="index.js"></script>
+</head>
+<body>
+    <article id="citation">La vie a beaucoup plus d'imagination que nous.</article>
+    <button id="button">Bouton</button>
+</body>
+</html>
